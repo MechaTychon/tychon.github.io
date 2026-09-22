6dc33a192dd09a72dfbfff9add1eca757d18c68b
Tychon <filiplarsson295@gmail.com>
2026-09-22 13:01:27 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+// Namnge cachen, öka versionsnumret när filerna ändras
+const CACHE_NAME = "min-pwa-cache-v1";
+
+// Filer som ska cachas direkt när service workern installeras
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Install: cacha grundfilerna
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activate: städa bort gamla cache-versioner
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: försök hämta från nätet, fall tillbaka på cache (offline-stöd)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
